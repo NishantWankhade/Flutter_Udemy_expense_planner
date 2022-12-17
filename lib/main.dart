ce87d63b5d6ab978f3673221f9257e9b4f3ada64
NGW334 <ngw334@gmail.com>
2022-12-17 15:19:41 +0530

    Adding DateFormat intl package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -84,7 +85,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat.yMMMd().format(tx.date),
                           style: const TextStyle(
                             color: Colors.grey,
                           ),
