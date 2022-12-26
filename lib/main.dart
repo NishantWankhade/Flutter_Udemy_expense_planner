6cde0f7739c397730b6e37615444a54b2135f0c8
NGW334 <ngw334@gmail.com>
2022-12-26 14:28:42 +0530

    -> main.dart     -> finding the height available         = MediaQuery.of(context).size.height -> (total size)         - MediaQuery.of(context).padding.top -> (Status bar)         - appBar.preferredSize.height -> (height of appBar)

--- lib/main.dart
+++ lib/main.dart
@@ -82,26 +82,38 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Personal Expenses',
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: (() {
-              _startAddNewTransaction(context);
-            }),
-          ),
-        ],
+    final appBar = AppBar(
+      title: const Text(
+        'Personal Expenses',
       ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: (() {
+            _startAddNewTransaction(context);
+          }),
+        ),
+      ],
+    );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransaction),
-            TransactionList(_userTransactions, _deleteTransaction),
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.4,
+                child: Chart(_recentTransaction)),
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.7,
+              child: TransactionList(_userTransactions, _deleteTransaction),
+            ),
           ],
         ),
       ),
