40ee880ab3e08ea091b0b398caa842cce3348f34
NGW334 <ngw334@gmail.com>
2022-12-26 14:34:08 +0530

    ...

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,65 +9,68 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return transactions.isEmpty
-        ? Column(
-            children: [
-              const Text(
-                "No Transactions added yet!!",
-                style: TextStyle(
-                  fontFamily: 'Quicksand',
-                  fontWeight: FontWeight.bold,
+    return Container(
+      height: 400,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                const Text(
+                  "No Transactions added yet!!",
+                  style: TextStyle(
+                    fontFamily: 'Quicksand',
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-            ],
-          )
-        : ListView.builder(
-            itemBuilder: (ctx, index) {
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 5,
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: EdgeInsets.all(6),
-                      child: FittedBox(
-                          child: Text('\$${transactions[index].amount}')),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text('\$${transactions[index].amount}')),
+                      ),
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Color.fromARGB(255, 255, 0, 0),
+                      onPressed: () => deleteTx(transactions[index].id),
                     ),
                   ),
-                  title: Text(
-                    transactions[index].title,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Quicksand',
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    color: Color.fromARGB(255, 255, 0, 0),
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
-                ),
-              );
-            },
-            itemCount: transactions.length,
-          );
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
   }
 }
